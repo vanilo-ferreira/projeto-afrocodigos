91924a0a4cb42220875729e743cb84371b9818ce
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-06-22 09:57:33 -0300

    feat: Importar  o FormsModule

--- src/app/create-collaborators/create-collaborators.module.ts
+++ src/app/create-collaborators/create-collaborators.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateCollaboratorsComponent } from './create-collaborators.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [CreateCollaboratorsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   exports: [CreateCollaboratorsComponent]
 })
 export class CreateCollaboratorsModule {}
