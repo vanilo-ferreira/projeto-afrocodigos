d7027e70d5b11acf07d07d11fb5eb5f264a2896f
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-06-22 12:08:00 -0300

    feat: Adicionar as interfaces RequestUpdate, ResponseCollaborator e ResponseUpdateCollaborator

--- src/app/app.model.ts
+++ src/app/app.model.ts
@@ -8,3 +8,22 @@ export interface Collaborators {
   name: string;
   role: string;
 }
+
+export interface RequestUpdate {
+  name: string;
+  role: string;
+}
+
+export interface ResponseCollaborator{
+  id: number;
+  created_at: number;
+  name: string;
+  role: string;
+}
+
+export interface ResponseUpdateCollaborator{
+  id: number;
+  created_at: number;
+  name: string;
+  role: string;
+}
