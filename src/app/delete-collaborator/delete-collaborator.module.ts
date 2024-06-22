208c1e144b67c1dd1ad701e6a156d791d3dd54a5
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-06-22 13:20:07 -0300

    feat: Adicionar o módulo DeleteCollaborator

---
+++ src/app/delete-collaborator/delete-collaborator.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DeleteCollaboratorComponent } from './delete-collaborator.component';
+
+
+
+@NgModule({
+  declarations: [DeleteCollaboratorComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [DeleteCollaboratorComponent],
+})
+export class DeleteCollaboratorModule { }
