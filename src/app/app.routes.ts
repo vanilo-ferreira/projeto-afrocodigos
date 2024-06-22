7109cb2569b674586b505244482364083a747cc9
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-06-21 23:55:40 -0300

    feat: Adicionar rota /register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { CollaboratorsComponent } from './collaborators/collaborators.component';
+import { CreateCollaboratorsComponent } from './create-collaborators/create-collaborators.component';
 
 export const routes: Routes = [
-    { path: "collaborators", component: CollaboratorsComponent},
+  { path: 'collaborators', component: CollaboratorsComponent },
+  { path: 'register', component: CreateCollaboratorsComponent },
 ];
