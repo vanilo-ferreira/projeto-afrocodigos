1a30bbd6dd3ac3139aa501e03799443b8a207a35
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-06-21 23:12:39 -0300

    fix: Adicionar configuração de rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports: [BrowserModule],
+  imports: [BrowserModule, AppRoutingModule],
   exports: [],
   declarations: [AppComponent],
   providers: [],
