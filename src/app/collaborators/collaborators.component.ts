3ec915d3d604ddd011def7b605dc248dab77d491
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-06-21 23:22:07 -0300

    feat: Adicionar CollaboratorsComponent

---
+++ src/app/collaborators/collaborators.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-collaborators',
+  templateUrl: './collaborators.component.html',
+  styleUrl: './collaborators.component.css'
+})
+export class CollaboratorsComponent {
+
+}
