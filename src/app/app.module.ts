f86afd981468c9e12979f99474bec06e4b9dbcf5
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-06-21 23:02:44 -0300

    feat: Adicionar appModule

---
+++ src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+  imports: [BrowserModule],
+  exports: [],
+  declarations: [AppComponent],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
