2f80d60cdacbffadf97257a06d1240f86ab4b48e
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-06-22 12:02:06 -0300

    feat: Importar o AppRoutingModule

--- src/app/collaborators/collaborators.module.ts
+++ src/app/collaborators/collaborators.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CollaboratorsComponent } from './collaborators.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 
 
 @NgModule({
   declarations: [CollaboratorsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ],
   exports: [CollaboratorsComponent]
 })
