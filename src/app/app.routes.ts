a7adec54ed4975d54c4fd79dcfbb2d3d518bfb6a
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-06-22 13:17:11 -0300

    feat: Adicionar a rota collaborators/delete/:id

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { CollaboratorsComponent } from './collaborators/collaborators.component';
 import { CreateCollaboratorsComponent } from './create-collaborators/create-collaborators.component';
 import { UpdateCollaboratorComponent } from './update-collaborator/update-collaborator.component';
+import { DeleteCollaboratorComponent } from './delete-collaborator/delete-collaborator.component';
 
 export const routes: Routes = [
   { path: 'collaborators', component: CollaboratorsComponent },
   { path: 'collaborators/register', component: CreateCollaboratorsComponent },
   { path: 'collaborators/update/:id', component: UpdateCollaboratorComponent },
+  { path: 'collaborators/delete/:id', component: DeleteCollaboratorComponent },
 ];
