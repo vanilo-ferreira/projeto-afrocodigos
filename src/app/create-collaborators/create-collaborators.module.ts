542bacd7338dd6dac255e63509cf37d050cae60a
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-06-21 23:55:15 -0300

    feat: Adicionar CreateCollaboratorsComponent

---
+++ src/app/create-collaborators/create-collaborators.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CreateCollaboratorsComponent } from './create-collaborators.component';
+
+@NgModule({
+  declarations: [CreateCollaboratorsComponent],
+  imports: [CommonModule],
+  exports: [CreateCollaboratorsComponent]
+})
+export class CreateCollaboratorsModule {}
