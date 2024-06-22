554a80272ebe665c3bca5a90a3d7865963e0737f
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-06-21 23:56:15 -0300

    feat: Importar CreateCollaboratorsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { CollaboratorsModule } from './collaborators/collaborators.module';
+import { CreateCollaboratorsModule } from './create-collaborators/create-collaborators.module';
 
 @NgModule({
-  imports: [BrowserModule, AppRoutingModule, CollaboratorsModule],
+  imports: [
+    BrowserModule, 
+    AppRoutingModule, 
+    CollaboratorsModule, 
+    CreateCollaboratorsModule
+  ],
   exports: [],
   declarations: [AppComponent],
   providers: [],
