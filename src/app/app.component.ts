adf34db255812be40f7996074656346a6e2f77d5
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-06-26 17:26:45 -0300

    feat: Ajustar acionamento de navegação para botões

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projeto-afrocodigos';
+
+  constructor (
+    private _route: Router
+  ) {}
+
+  register() {
+    this._route.navigate(['/collaborators/register']);
+  }
+
+  list() {
+    this._route.navigate(['/collaborators']);
+  }
 }
